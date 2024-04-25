6fc73a5c5f913862a899eb6769602a8de16b9b3d
Rory Hayes <roryhayes@computingforall.org>
2024-04-25 16:01:59 -0700

    adjust nav bar and footer css

--- hackathon/src/components/Footer/Footer.js
+++ hackathon/src/components/Footer/Footer.js
@@ -1,17 +1,22 @@
 import './Footer.css';
 
 export default function Footer() {
-    return (
-      <div className="footer justify-items-center py-4">
-        <div className="text-xs text-gray-300">
+
+  const footerStyle = {
+    display:"inline-block"
+  }
+
+  return (
+      <div className="footer justify-items-center py-2">
+        <div className="text-sm text-gray-300" style={footerStyle}>
           <p>Created by the Students of</p>
           <p className="text-gray-200 hover:text-gray-500 font-semibold"><a target="_blank" href="https://www.computingforall.org/pre-apprenticeship/">Computing for All's Pre-Apprenticeship Program</a></p>
           <p>Level 6 Group 1 Spring 2024!</p>
         </div>
-        <div className="py-2">
-          <a className="text-sm text-gray-200 hover:text-gray-500 px-8 font-semibold" target="_blank" href="https://github.com/orgs/PAP-2024-Spring-L6-G1/projects/2/views/1">&#123; Project Status Board &#125;</a>
-          <a className="text-sm text-gray-200 hover:text-gray-500 px-8 font-semibold" target="_blank" href="https://github.com/PAP-2024-Spring-L6-G1/Hackathon/deployments">&#123; Check Deployments &#125;</a>
-          <a className="text-sm text-gray-200 hover:text-gray-500 px-8 font-semibold" target="_blank" href="https://github.com/PAP-2024-Spring-L6-G1/Hackathon">&#123; View GitHub Repo &#125;</a>
+        <div className="text-sm text-gray-200 px-20 font-semibold" style={footerStyle}>
+          <p><a className="hover:text-gray-500" target="_blank" href="https://github.com/orgs/PAP-2024-Spring-L6-G1/projects/2/views/1">☑️ Project Status Board</a></p>
+          <p><a className="hover:text-gray-500" target="_blank" href="https://github.com/PAP-2024-Spring-L6-G1/Hackathon/deployments">☁️ Check Deployments</a></p>
+          <p><a className="hover:text-gray-500" target="_blank" href="https://github.com/PAP-2024-Spring-L6-G1/Hackathon">🧑‍💻 View GitHub Repo</a></p>
         </div>
       </div>
     );
