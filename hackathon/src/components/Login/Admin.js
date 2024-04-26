e47b37c229f7f4a9e6fc96992f7178fc916add29
Rory Hayes <roryhayes@computingforall.org>
2024-04-25 17:52:24 -0700

    init admin panel

--- hackathon/src/components/Login/Admin.js
+++ hackathon/src/components/Login/Admin.js
@@ -1,9 +1,18 @@
 export default function Admin() {
 
   return (
-    <div className="Admin">
-      {/* Content */} 
-      <p className='text-white'>Admin Page Goes Here</p>
+    <div className="Admin text-gray-200 flex justify-center">
+      <form className='mt-6'>
+      <h1 className="text-3xl text-white block text-center mb-6">⚠️ Administrator Panel ⚠️</h1>
+        <label className='block mb-6'>Please use caution when changing items below:</label>
+        <label>New countdown deadline datetime:
+          <input className='text-black block' type="datetime-local" />
+        </label><br></br>
+        <label>Allow submissions:
+          <input className='text-black' type="checkbox" />
+        </label><br></br>
+        <button className='mt-6 lg:inline-block py-2 px-6 lg:mr-3 bg-red-500 hover:bg-red-800 text-sm text-white font-semibold rounded-xl transition duration-200'>Submit</button>
+      </form>
     </div>
   );
 }
